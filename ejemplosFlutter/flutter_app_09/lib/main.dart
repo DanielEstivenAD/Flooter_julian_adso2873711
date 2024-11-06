20d9715af07201c9102fce0306484a8f24212ca2
DanielEstivenAD <danifra14once@gmail.com>
2024-11-06 07:27:57 -0500

    update

---
+++ ejemplosFlutter/flutter_app_09/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myhome());
+}
+
+class Myhome extends StatefulWidget {
+  const Myhome({super.key});
+
+  @override
+  State<Myhome> createState() => Myhome();
+}
+
+class Myhome extends State<Myhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+} {
+  const Myhome({super.key});
+}
+  // This widget is the root of your application.
